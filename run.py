888781b640ed9b0b463b251583927d93078b2be7
Mark Baggett <mbagget1@utk.edu>
2018-03-27 15:59:59 -0400

    Refactor harvest metadata to be operation agnostic.

--- run.py
+++ run.py
@@ -2,12 +2,26 @@ import yaml
 import argparse
 from fedora import Set
 
+def choose_operation(choice, instance, ds=None):
+    if choice == "grab_binaries":
+        instance.grab_binaries(ds)
+    elif choice == "update_gsearch":
+        instance.update_gsearch()
+    elif choice == "harvest_metadata":
+        instance.harvest_metadata(ds)
+    else:
+        print("No valid operator.")
+
+
+
 def main():
     parser = argparse.ArgumentParser(description='Use to specify a collection')
     parser.add_argument("-p", "--parentnamespace", dest="parent_namespace", help="parent namespace of collection")
     parser.add_argument("-dc", "--dcfield", dest="dc_field", help="grab pids according to dc field")
     parser.add_argument("-dcs", "--dcstring", dest="dc_string", help="specify a dc string")
     parser.add_argument("-ds", "--dsid", dest="datastream_id", help="specify text datastream.")
+    parser.add_argument("-o", "--operation", dest="operation", help="Choose one: grab_binaries, harvest_metadata, "
+                                                                    "update_gsearch", required=True)
     args = parser.parse_args()
 
     settings = yaml.load(open("config.yml", "r"))
@@ -18,6 +32,7 @@ def main():
         fedora_url = f"http://{fedora_url}"
     if args.parent_namespace:
         fedora_collection = f"pid%7E{args.parent_namespace}*"
+    operation = args.operation
     if args.dc_field and args.dc_string:
         dc_parameter = f"{args.dc_field}%7E%27{args.dc_string}%27"
     elif args.dc_field or args.dc_string:
@@ -30,7 +45,6 @@ def main():
                  f"&pid=true&resultFormat=xml".replace(" ", "%20")
     my_records = Set(my_request, settings)
     my_records.populate()
-    my_records.harvest_metadata(dsid)
-    print(my_records.size_of_set())
+    choose_operation(operation, my_records, dsid)
 
 if __name__ == "__main__": main()
