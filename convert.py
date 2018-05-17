de7eeeef8aa60654e941b8884004edfd31933cb1
Mark Baggett <mbagget1@utk.edu>
2018-05-17 10:17:48 -0400

    Extend for multiple operations.

--- convert.py
+++ convert.py
@@ -1,6 +1,8 @@
 from subprocess import call
 import yaml
 import os
+import argparse
+
 
 class Image:
     def __init__(self, name):
@@ -16,13 +18,27 @@ class Image:
         print(f"Converting {self.name} to {output}{extension}.")
         call(f"convert -thumbnail x250 -alpha remove '{self.name}[0]' {output}.jpg", shell=True)
 
+    def preview_to_thumb(self):
+        output = self.name.split('.', maxsplit=1)[0]
+        print(f"Converting {self.name} to {output}{extension}.")
+        call(f"convert -thumbnail x600 -alpha remove '{self.name}[0]' {output}.jpg", shell=True)
+
 
 def main():
+    parser = argparse.ArgumentParser(description='Specify operation')
+    parser.add_argument("-o", "--operation", dest="operation", help="Choose one: pdf_preview, pdf_thumb, thumb",
+                        default="thumb")
+    args = parser.parse_args()
     settings = yaml.load(open("config.yml", "r"))
     for path in os.walk(settings["destination_directory"]):
         for file in path[2]:
             img = Image(f"{settings['destination_directory']}/{file}")
-            img.convert()
+            if args.operation == "pdf_thumb":
+                img.pdf_to_thumb()
+            elif args.operation == "pdf_preview":
+                img.preview_to_thumb()
+            else:
+                img.convert()
 
 if __name__ == "__main__":
     main()
