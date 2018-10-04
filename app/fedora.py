4eb50eac1177ed329d5364b46daa12d4eac5b327
Mark Baggett <mbagget1@utk.edu>
2018-10-04 08:21:50 -0400

    Test hasModel.

--- app/fedora.py
+++ app/fedora.py
@@ -356,7 +356,8 @@ class Record:
             return f"Failed to purge {dsid} on {self.pid} with {r.status_code}.\n\n{temp_request}"
 
     def find_content_type(self):
-        predicate = "&predicate=info:fedora/fedora-system:def/relations-external#".replace(":", "%3a").replace("/", "%2f").replace("#", "%23")
+        predicate = "&predicate=info:fedora/fedora-system:def/relations-external#" \
+                    "hasModel".replace(":", "%3a").replace("/", "%2f").replace("#", "%23")
         r = requests.get(f"{self.settings['fedora_path']}:{self.settings['port']}/fedora/objects/"
                          f"{self.pid}/relationships?subject=info%3afedora%2f{self.pid}&format=turtle{predicate}",
                          auth=(f"{self.settings['username']}", f"{self.settings['password']}"))
