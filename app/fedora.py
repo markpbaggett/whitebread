9e6d94542af0efa1a3e318a05aa9e1e2eb5d3367
Mark Baggett <mbagget1@utk.edu>
2019-07-05 11:22:03 -0400

    Convert string to dict.

--- app/fedora.py
+++ app/fedora.py
@@ -162,22 +162,21 @@ class Set:
                              f"datastreams/{dsid}/history?format=xml",
                              auth=(self.settings['username'], self.settings['password']))
             if r.status_code == 200:
-                json_response = json.dumps(xmltodict.parse(r.text)['datastreamHistory'])
-                print(json_response['datastreamProfile'])
-                # for version in json_response['datastreamProfile']:
-                #     print(version)
-                    # version_title = version['dsVersionID'].replace('.', '_')
-                    # current_version = requests.get(f"{self.settings['fedora_path']}:{self.settings['port']}/fedora/"
-                    #                                f"objects/{result}/datastreams/{dsid}/content?asOfDateTime="
-                    #                                f"{version['dsCreateDate']}",
-                    #                                auth=(self.settings['username'], self.settings['password']))
-                    # if current_version.status_code == 200:
-                    #     new_name = result.replace(":", "_")
-                    #     ext = r.headers["Content-Type"].split(";")[0].split("/")[1]
-                    #     with open(f"{self.settings['destination_directory']}/{new_name}{version_title}.{ext}", 'wb') as other:
-                    #         other.write(r.content)
-                    # else:
-                    #     print(f"Failed to download {dsid} for {result} with {current_version.status_code}.")
+                json_response = json.loads(json.dumps(xmltodict.parse(r.text)['datastreamHistory']))
+                for version in json_response['datastreamProfile']:
+                    print(version)
+                    version_title = version['dsVersionID'].replace('.', '_')
+                    current_version = requests.get(f"{self.settings['fedora_path']}:{self.settings['port']}/fedora/"
+                                                   f"objects/{result}/datastreams/{dsid}/content?asOfDateTime="
+                                                   f"{version['dsCreateDate']}",
+                                                   auth=(self.settings['username'], self.settings['password']))
+                    if current_version.status_code == 200:
+                        new_name = result.replace(":", "_")
+                        ext = r.headers["Content-Type"].split(";")[0].split("/")[1]
+                        with open(f"{self.settings['destination_directory']}/{new_name}{version_title}.{ext}", 'wb') as other:
+                            other.write(r.content)
+                    else:
+                        print(f"Failed to download {dsid} for {result} with {current_version.status_code}.")
             return
 
     def size_of_set(self):
